e8e8adc2661feb8474e864360721f3e905cdb8df
Jeremy Woertink <jeremywoertink@gmail.com>
2017-06-02 21:45:34 -0700

    writing some notes about loading tiles

--- src/crono/image.cr
+++ src/crono/image.cr
@@ -4,11 +4,23 @@ module Crono
     @sdl_img : SDL::Texture
     property src, dimentions
 
-    # May need to implement SDL_CreateTextureFromSurface for this to work
     def self.load_tiles(src : String, dimentions : TSize)
-      #img = new(src)
-      #Crono.renderer.sdl.copy(img.sdl, srcrect: {0, 0, 200, 50}, dstrect: {0, 0, 50, 50})
-      #SDL::Surface.load_texture(Crono.renderer.sdl, img.sdl)
+      img = self.new(src, dimentions)
+      sprite = SDL::Texture.from(img, Crono.renderer.not_nil!.sdl)
+      # img.width, img.height
+      # have the sprite size by dimentions[0] and dimentions[1]
+      # need to calculate how many sprites can be pulled from this size
+      # if the img is 200x25 and the dimentions are {25, 25}
+      # then the total amount of sprites would be 8
+      # (width / d[0]) * (height / d[1])
+      # (200   / 25)   * (25     / 25)
+      # What happens if the image is a wonky size like 326x48
+      # (326 / 25)13.04 * (48 / 25)1.92 = 25.036....
+      # If we floor the values it would be 13. That's a big difference
+      # We need this to return some sort of collection. It should have the img and
+      # all of the sprite locations which would be a SDL::Rect
+      # Maybe use it like this?
+      # render.copy(sprite[0].image, sprite[0].clip, location_to_draw)
     end
 
     def initialize(@src : String, @dimentions : TSize)
