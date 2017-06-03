637ae0e919f310f6861c226fe493da1bf126eb30
Jeremy Woertink <jeremywoertink@gmail.com>
2017-06-02 20:22:42 -0700

    fixed the offset_y and offset_x methods. added new math methods

--- src/crono/math.cr
+++ src/crono/math.cr
@@ -1,14 +1,24 @@
 module Crono
   module GameMath
     
-    def offset_x(theta : Int32, magnitude : Float64)
-      radians = theta * Math::PI / 180
-      magnitude * Math.cos(radians)
+    def offset_x(angle : Int32, radius : Float64)
+      Math.sin(angle / 180 * Math::PI) * radius
     end
 
-    def offset_y(theta : Int32, magnitude : Float64)
-      radians = theta * Math::PI / 180
-      magnitude * Math.sin(radians)
+    def offset_y(angle : Int32, radius : Float64)
+      -Math.cos(angle / 180 * Math::PI) * radius 
+    end
+
+    def distance(x1 : Float64, y1 : Float64, x2 : Float64, y2 : Float64)
+      Math.sqrt(distance_sqr(x1, y1, x2, y2))
+    end
+
+    def distance_sqr(x1 : Float64, y1 : Float64, x2 : Float64, y2 : Float64)
+      square(x1 - x2) + square(y1 - y2)
+    end
+
+    def square(value : Int32 | Float64)
+      value * value
     end
   end
 end
