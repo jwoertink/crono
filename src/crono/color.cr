f0ff8cd1e8df1a8ffb7228db7800b719bcf844b0
Jeremy Woertink <jeremywoertink@gmail.com>
2017-06-11 10:43:27 -0700

    updated color struct with new rand method. Also renamed alias to better fit what it is

--- src/crono/color.cr
+++ src/crono/color.cr
@@ -17,34 +17,39 @@ module Crono
     MAGENTA = {255, 0, 255, 255}
     PINK = {255, 0, 171, 255}
   
-    alias TColor = Tuple(Int32, Int32, Int32, Int32)
+    alias RGBA = Tuple(Int32, Int32, Int32, Int32)
+    alias RGB = Tuple(Int32, Int32, Int32)
 
-    def self.to_hex(values : Tuple(Int32, Int32, Int32))
+    def self.to_hex(values : RGB) 
       to_hex({values[0], values[1], values[2], 255})
     end
 
-    def self.to_hex(values : TColor)
+    def self.to_hex(values : RGBA)
       "#%02X%02X%02X%02X" % values
     end
 
-    # Returns `TColor` with all non-zero values lowered by percent.
+    # Returns `RGBA` with all non-zero values lowered by percent.
     # Does not change transparency
-    def self.darken(values : TColor, percent : Int32)
+    def self.darken(values : RGBA, percent : Int32)
       numbers = [] of Int32
       numbers << (values[0] - (values[0] * percent) / 100)
       numbers << (values[1] - (values[1] * percent) / 100)
       numbers << (values[2] - (values[2] * percent) / 100)
       numbers << values[3]
-      TColor.from(numbers)
+      RGBA.from(numbers)
     end
 
-    def self.lighten(values : TColor, percent : Int32)
+    def self.lighten(values : RGBA, percent : Int32)
       numbers = [] of Int32
       numbers << (values[0] + (255 * percent) / 100)
       numbers << (values[1] + (255 * percent) / 100)
       numbers << (values[2] + (255 * percent) / 100)
       numbers << values[3]
-      TColor.from(numbers)
+      RGBA.from(numbers)
+    end
+
+    def self.rand
+      RGB.from([rand(256), rand(256), rand(256)])
     end
   end
 end
