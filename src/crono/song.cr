a602d8b46458fddf669243544f7019541f87fbff
Jeremy Woertink <jeremywoertink@gmail.com>
2017-06-16 15:57:33 -0700

    added a song class to play music

---
+++ src/crono/song.cr
@@ -0,0 +1,41 @@
+module Crono
+  class Song
+    @sdl : SDL::Mix::Music
+    delegate playing?, paused?, to: @sdl
+    
+    def initialize(@src : String)
+      @sdl = SDL::Mix::Music.new(@src)
+    end
+
+    def play
+      return if playing?
+      @sdl.play
+    end
+
+    def pause
+      return if paused?
+      @sdl.pause
+    end
+
+    def resume
+      return if playing?
+      @sdl.resume
+    end
+
+    # Toggle music between playing and pausing
+    def toggle
+      if paused?
+        resume
+      elsif playing?
+        pause
+      else
+        play
+      end
+    end
+
+    def stop
+      resume if paused?
+      @sdl.stop
+    end
+  end
+end
