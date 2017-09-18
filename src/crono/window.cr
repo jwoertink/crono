6e4c5db94fb2f1225ff6898e10a0f6dc95135bc9
Jeremy Woertink <jeremywoertink@gmail.com>
2017-09-17 20:23:46 -0700

    still working out a new game loop with smoother animation. Animations still feel choppy

--- src/crono/window.cr
+++ src/crono/window.cr
@@ -47,6 +47,12 @@ module Crono
       @sdl_window.not_nil!
     end
 
+    def handle_event(event)
+      key_pressed(event.sym) if event.pressed?
+      key_down(event.sym) if event.keydown?
+      key_up(event.sym) if event.keyup?
+    end
+
     private def init_sdl!
       @sdl_window = SDL::Window.new(title, width, height)
       Crono.renderer = Crono::Renderer.new(sdl)
@@ -57,50 +63,18 @@ module Crono
     end
 
     private def run_game_loop
-      update_interval = 120
-      draw_interval = 60
-      time_of_last_update = current_time
-      time_of_last_draw = current_time
       loop do
         case event = SDL::Event.poll
         when SDL::Event::Quit
           break
         when SDL::Event::Keyboard
-          key_pressed(event.sym) if event.pressed?
-          key_down(event.sym) if event.keydown?
-          key_up(event.sym) if event.keyup?
+          handle_event(event)
         end
 
-        time_until_update = (update_interval + time_of_last_update) - current_time
-        time_until_draw = (draw_interval + time_of_last_draw) - current_time
-        work_done = false
-
-        if time_until_update <= 0
-          update
-          #sdl.update # Do I need this?
-          Crono.renderer.sdl.present
-          time_of_last_update = current_time
-          work_done = true
-        end
-
-        if time_until_draw <= 0
-          clear_screen
-          draw
-          time_of_last_draw = current_time
-          work_done = true
-        end
-
-        if work_done == false
-          sleep_seconds = time_until_update
-
-          if time_until_draw < sleep_seconds
-            sleep_seconds = time_until_draw
-          end
-          time_to_sleep = sleep_seconds * 0.001
-          sleep(time_to_sleep)
-        end
-       
-        break if @close_window
+        update
+        clear_screen
+        draw
+        Crono.renderer.present
       end
     end
 
